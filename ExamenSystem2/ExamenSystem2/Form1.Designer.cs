3ff01f4e39107577ff41c7c364bcb5531a92079d
Andrii Sichovyi <48842271+wayofthesamurai@users.noreply.github.com>
2021-07-06 13:24:31 +0300

    Words_Seacher_In_Files

---
+++ ExamenSystem2/ExamenSystem2/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace ExamenSystem2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Start = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Continue = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.ShowDrivers = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(455, 12);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(91, 32);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 256);
+            this.textBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 412);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(617, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(541, 274);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(88, 132);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(281, 441);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(88, 33);
+            this.Pause.TabIndex = 4;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(541, 441);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(88, 33);
+            this.Cancel.TabIndex = 5;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Continue
+            // 
+            this.Continue.Location = new System.Drawing.Point(12, 441);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(88, 33);
+            this.Continue.TabIndex = 6;
+            this.Continue.Text = "Continue";
+            this.Continue.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(455, 94);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(174, 174);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // ShowDrivers
+            // 
+            this.ShowDrivers.Location = new System.Drawing.Point(455, 55);
+            this.ShowDrivers.Name = "ShowDrivers";
+            this.ShowDrivers.Size = new System.Drawing.Size(174, 33);
+            this.ShowDrivers.TabIndex = 8;
+            this.ShowDrivers.Text = "Show Drivers";
+            this.ShowDrivers.UseVisualStyleBackColor = true;
+            this.ShowDrivers.Click += new System.EventHandler(this.ShowDrivers_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 274);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(522, 132);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 486);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ShowDrivers);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.Continue);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button Continue;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button ShowDrivers;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ExamenSystem2/ExamenSystem2/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ExamenSystem2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Directory.CreateDirectory(@"D:\ReportProgramm\");
+            textBox2.Text = "В программе реализовано: поиск файлов формата .txt и их запись в файл отчёта, + поиск запретных слов в этих же найденых файлах, создание их копий в отдельнюю папку на диску D (ReportProgramm и файлы отчёта FindWordsInFiles,FoundFile), так же копии файлов з заменёнными запретными словами на *******. Не реализовано: кнопки Pause, Continue, Cancel, многопоточность и синхронизация и открытие програмы через консоль";
+            progressBar1.Maximum = 1000;
+        }
+        public StreamWriter Write = new StreamWriter(@"D:\FoundFile.txt");
+        public StreamWriter FindWord = new StreamWriter(@"D:\FindWordsInFiles.txt");
+
+        public string[] names;
+        public int i = 0;
+        public string DriversName;
+
+        private void ShowDrivers_Click(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            int i = 0;
+            foreach (DriveInfo d in allDrives)
+            {
+                checkedListBox1.Items.Add(d.Name);
+                checkedListBox1.SetItemChecked(i, true);
+                i++;
+            }
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "D:\\";
+            openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)||*.*|";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog.FileName;
+            textBox1.Text = File.ReadAllText(filename);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            names = new string[textBox1.Lines.Count()];
+            for (; i < names.Length; i++)
+                names[i] = textBox1.Lines[i];           
+            foreach (object itemChecked in checkedListBox1.CheckedItems)
+            {
+                string NameDrivers = itemChecked.ToString();
+                GetRecursFiles(NameDrivers);
+            }
+            progressBar1.Value = 1000;
+            Write.Close();
+            FindWord.Close();
+            progressBar1.Value = 0;
+            MessageBox.Show("Work Finish");
+        }
+
+        public void GetRecursFiles(string start_path)
+        {
+            try
+            {
+                string[] folders = Directory.GetDirectories(start_path);
+                foreach (string folder in folders)
+                {
+                    if (folder != @"D:\ReportProgramm\")
+                        GetRecursFiles(folder);
+                }
+                string[] files = Directory.GetFiles(start_path, "*.txt");
+                foreach (string filename in files)
+                {
+                    Write.WriteLine("File: " + filename);
+                    FindWords(filename);
+                    progressBar1.Value += 1;
+                }
+            }
+            catch (System.Exception e)
+            {
+                //MessageBox.Show(e.Message);
+            }
+        }
+        public void FindWords(string filename)
+        {
+            bool Yes = false;
+            string text = new StreamReader(filename).ReadToEnd(); // открывает файл
+            string[] words = text.Split(new char[] { ' ', ',', '.', '\r', '\n', '\t', '\\', '!', '?', '#', '(', ')', '*', '$', '@' });
+            int k = words.Length;
+            int f = 0;
+            int j = 0;
+            int[] count = new int[i];
+            for (; j < i; j++)
+            {
+                for (int t = 0; t < k; t++)
+                {
+                    if (words[t] == names[j])
+                    {
+                        count[j]++;
+                    }
+                }
+                if (count[j] > 0)
+                {
+                    f++;
+                    if (f == 1)
+                        FindWord.Write(filename + ": have - ");
+                    if (f >= 1)
+                    {
+                        FindWord.Write(names[j] + '(' + count[j] + ") ");
+                        Yes = true;
+                    }
+                }
+            }
+            if (Yes == true)
+            {
+                FindWord.WriteLine();
+                Copy(filename);
+                EditCopy(filename);                
+            }
+        }
+
+        public void Copy(string PathAndFileName)
+        {
+            string OnlyFileName = Path.GetFileNameWithoutExtension(PathAndFileName);
+            string newpath = @"D:\ReportProgramm\CopyFilesFromProgramm\";
+            string newFileName = OnlyFileName+"_copy";
+            FileInfo f1 = new FileInfo(PathAndFileName);
+            if (f1.Exists)
+            {
+                if (!Directory.Exists(newpath))
+                {
+                    Directory.CreateDirectory(newpath);
+                }
+                f1.CopyTo(string.Format("{0}{1}{2}", newpath, newFileName, f1.Extension));               
+            }         
+        }
+
+        public void EditCopy(string filename)
+        {
+            string OnlyFileName = Path.GetFileNameWithoutExtension(filename);
+            string newpath2 = @"D:\ReportProgramm\EditFiles\";
+            if (!Directory.Exists(newpath2))
+            {
+                Directory.CreateDirectory(newpath2);
+            }
+        
+            string EditText = new StreamReader(filename).ReadToEnd(); // открывает файл
+            for (int t = 0; t < i; t++)
+            {
+                EditText = EditText.Replace(names[t], "*******");
+            }
+            StreamWriter e = new StreamWriter(@"D:\ReportProgramm\EditFiles\"+OnlyFileName+"_edit.txt");
+            e.Write(EditText);
+            e.Close();
+        }     
+    }
+}
