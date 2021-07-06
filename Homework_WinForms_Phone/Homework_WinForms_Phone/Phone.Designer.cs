d3ae18cad9e852d8a74e7bd56bc5a2f362cba84f
Andrii Sichovyi <48842271+wayofthesamurai@users.noreply.github.com>
2021-07-06 13:10:19 +0300

    Add files via upload

---
+++ Homework_WinForms_Phone/Homework_WinForms_Phone/Phone.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace Homework_WinForms_Phone
+{
+    partial class Phone
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Phone));
+            this.display = new System.Windows.Forms.TextBox();
+            this.Up = new System.Windows.Forms.Button();
+            this.Left = new System.Windows.Forms.Button();
+            this.Right = new System.Windows.Forms.Button();
+            this.Down = new System.Windows.Forms.Button();
+            this.One = new System.Windows.Forms.Button();
+            this.abc2 = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.ADD = new System.Windows.Forms.Button();
+            this.def3 = new System.Windows.Forms.Button();
+            this.ghi4 = new System.Windows.Forms.Button();
+            this.jkl5 = new System.Windows.Forms.Button();
+            this.tuv8 = new System.Windows.Forms.Button();
+            this.ZeroSpace = new System.Windows.Forms.Button();
+            this.mno6 = new System.Windows.Forms.Button();
+            this.pqrs7 = new System.Windows.Forms.Button();
+            this.wxyz9 = new System.Windows.Forms.Button();
+            this.StarPlus = new System.Windows.Forms.Button();
+            this.Hesh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(13, 13);
+            this.display.Multiline = true;
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(222, 135);
+            this.display.TabIndex = 0;
+            // 
+            // Up
+            // 
+            this.Up.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Up.Image = ((System.Drawing.Image)(resources.GetObject("Up.Image")));
+            this.Up.Location = new System.Drawing.Point(99, 166);
+            this.Up.Name = "Up";
+            this.Up.Size = new System.Drawing.Size(50, 50);
+            this.Up.TabIndex = 1;
+            this.Up.UseVisualStyleBackColor = false;
+            this.Up.Click += new System.EventHandler(this.Up_Click);
+            // 
+            // Left
+            // 
+            this.Left.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Left.Image = ((System.Drawing.Image)(resources.GetObject("Left.Image")));
+            this.Left.Location = new System.Drawing.Point(43, 193);
+            this.Left.Name = "Left";
+            this.Left.Size = new System.Drawing.Size(50, 50);
+            this.Left.TabIndex = 2;
+            this.Left.UseVisualStyleBackColor = false;
+            // 
+            // Right
+            // 
+            this.Right.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Right.Image = ((System.Drawing.Image)(resources.GetObject("Right.Image")));
+            this.Right.Location = new System.Drawing.Point(155, 193);
+            this.Right.Name = "Right";
+            this.Right.Size = new System.Drawing.Size(50, 50);
+            this.Right.TabIndex = 3;
+            this.Right.UseVisualStyleBackColor = false;
+            // 
+            // Down
+            // 
+            this.Down.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Down.Image = ((System.Drawing.Image)(resources.GetObject("Down.Image")));
+            this.Down.Location = new System.Drawing.Point(99, 222);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(50, 50);
+            this.Down.TabIndex = 4;
+            this.Down.UseVisualStyleBackColor = false;
+            // 
+            // One
+            // 
+            this.One.Location = new System.Drawing.Point(13, 287);
+            this.One.Name = "One";
+            this.One.Size = new System.Drawing.Size(70, 70);
+            this.One.TabIndex = 5;
+            this.One.Text = "1";
+            this.One.UseVisualStyleBackColor = true;
+            this.One.Click += new System.EventHandler(this.One_Click);
+            // 
+            // abc2
+            // 
+            this.abc2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.abc2.Location = new System.Drawing.Point(89, 287);
+            this.abc2.Name = "abc2";
+            this.abc2.Size = new System.Drawing.Size(70, 70);
+            this.abc2.TabIndex = 17;
+            this.abc2.Text = " 2   АБВГ ABC";
+            this.abc2.UseVisualStyleBackColor = true;
+            this.abc2.Click += new System.EventHandler(this.abc2_Click);
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(13, 249);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(70, 32);
+            this.C.TabIndex = 18;
+            this.C.Text = "С";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // ADD
+            // 
+            this.ADD.Location = new System.Drawing.Point(165, 249);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(70, 32);
+            this.ADD.TabIndex = 19;
+            this.ADD.Text = "ADD";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // def3
+            // 
+            this.def3.Location = new System.Drawing.Point(165, 287);
+            this.def3.Name = "def3";
+            this.def3.Size = new System.Drawing.Size(70, 70);
+            this.def3.TabIndex = 20;
+            this.def3.Text = " 3 ДЕЖЗ DEF";
+            this.def3.UseVisualStyleBackColor = true;
+            this.def3.Click += new System.EventHandler(this.def3_Click);
+            // 
+            // ghi4
+            // 
+            this.ghi4.Location = new System.Drawing.Point(13, 363);
+            this.ghi4.Name = "ghi4";
+            this.ghi4.Size = new System.Drawing.Size(70, 70);
+            this.ghi4.TabIndex = 21;
+            this.ghi4.Text = " 4 ИЙКЛ GHI";
+            this.ghi4.UseVisualStyleBackColor = true;
+            this.ghi4.Click += new System.EventHandler(this.ghi4_Click);
+            // 
+            // jkl5
+            // 
+            this.jkl5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.jkl5.Location = new System.Drawing.Point(89, 363);
+            this.jkl5.Name = "jkl5";
+            this.jkl5.Size = new System.Drawing.Size(70, 70);
+            this.jkl5.TabIndex = 22;
+            this.jkl5.Text = "5 МНОП JKL";
+            this.jkl5.UseVisualStyleBackColor = true;
+            this.jkl5.Click += new System.EventHandler(this.jkl5_Click);
+            // 
+            // tuv8
+            // 
+            this.tuv8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tuv8.Location = new System.Drawing.Point(89, 439);
+            this.tuv8.Name = "tuv8";
+            this.tuv8.Size = new System.Drawing.Size(70, 70);
+            this.tuv8.TabIndex = 23;
+            this.tuv8.Text = "8 ШЩЪЫ TUV";
+            this.tuv8.UseVisualStyleBackColor = true;
+            this.tuv8.Click += new System.EventHandler(this.tuv8_Click);
+            // 
+            // ZeroSpace
+            // 
+            this.ZeroSpace.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ZeroSpace.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ZeroSpace.Location = new System.Drawing.Point(89, 515);
+            this.ZeroSpace.Name = "ZeroSpace";
+            this.ZeroSpace.Size = new System.Drawing.Size(70, 70);
+            this.ZeroSpace.TabIndex = 24;
+            this.ZeroSpace.Text = "     0       _";
+            this.ZeroSpace.UseVisualStyleBackColor = false;
+            this.ZeroSpace.Click += new System.EventHandler(this.ZeroSpace_Click);
+            // 
+            // mno6
+            // 
+            this.mno6.Location = new System.Drawing.Point(165, 363);
+            this.mno6.Name = "mno6";
+            this.mno6.Size = new System.Drawing.Size(70, 70);
+            this.mno6.TabIndex = 25;
+            this.mno6.Text = "  6 РСТУ MNO";
+            this.mno6.UseVisualStyleBackColor = true;
+            this.mno6.Click += new System.EventHandler(this.mno6_Click);
+            // 
+            // pqrs7
+            // 
+            this.pqrs7.Location = new System.Drawing.Point(13, 439);
+            this.pqrs7.Name = "pqrs7";
+            this.pqrs7.Size = new System.Drawing.Size(70, 70);
+            this.pqrs7.TabIndex = 26;
+            this.pqrs7.Text = " 7 ФХЦЧ PQRS";
+            this.pqrs7.UseVisualStyleBackColor = true;
+            this.pqrs7.Click += new System.EventHandler(this.pqrs7_Click);
+            // 
+            // wxyz9
+            // 
+            this.wxyz9.Location = new System.Drawing.Point(165, 439);
+            this.wxyz9.Name = "wxyz9";
+            this.wxyz9.Size = new System.Drawing.Size(70, 70);
+            this.wxyz9.TabIndex = 27;
+            this.wxyz9.Text = "9 ЬЭЮЯ WXYZ";
+            this.wxyz9.UseVisualStyleBackColor = true;
+            this.wxyz9.Click += new System.EventHandler(this.wxyz9_Click);
+            // 
+            // StarPlus
+            // 
+            this.StarPlus.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.StarPlus.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StarPlus.Location = new System.Drawing.Point(13, 515);
+            this.StarPlus.Name = "StarPlus";
+            this.StarPlus.Size = new System.Drawing.Size(70, 70);
+            this.StarPlus.TabIndex = 28;
+            this.StarPlus.Text = "   *     +";
+            this.StarPlus.UseVisualStyleBackColor = false;
+            this.StarPlus.Click += new System.EventHandler(this.StarPlus_Click);
+            // 
+            // Hesh
+            // 
+            this.Hesh.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Hesh.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Hesh.Location = new System.Drawing.Point(165, 515);
+            this.Hesh.Name = "Hesh";
+            this.Hesh.Size = new System.Drawing.Size(70, 70);
+            this.Hesh.TabIndex = 29;
+            this.Hesh.Text = "#";
+            this.Hesh.UseVisualStyleBackColor = false;
+            this.Hesh.Click += new System.EventHandler(this.Hesh_Click);
+            // 
+            // Phone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(247, 603);
+            this.Controls.Add(this.Hesh);
+            this.Controls.Add(this.StarPlus);
+            this.Controls.Add(this.wxyz9);
+            this.Controls.Add(this.pqrs7);
+            this.Controls.Add(this.mno6);
+            this.Controls.Add(this.ZeroSpace);
+            this.Controls.Add(this.tuv8);
+            this.Controls.Add(this.jkl5);
+            this.Controls.Add(this.ghi4);
+            this.Controls.Add(this.def3);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.abc2);
+            this.Controls.Add(this.One);
+            this.Controls.Add(this.Down);
+            this.Controls.Add(this.Right);
+            this.Controls.Add(this.Left);
+            this.Controls.Add(this.Up);
+            this.Controls.Add(this.display);
+            this.Name = "Phone";
+            this.Text = "Phone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox display;
+        private System.Windows.Forms.Button Up;
+        private System.Windows.Forms.Button Left;
+        private System.Windows.Forms.Button Right;
+        private System.Windows.Forms.Button Down;
+        private System.Windows.Forms.Button One;
+        private System.Windows.Forms.Button abc2;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.Button def3;
+        private System.Windows.Forms.Button ghi4;
+        private System.Windows.Forms.Button jkl5;
+        private System.Windows.Forms.Button tuv8;
+        private System.Windows.Forms.Button ZeroSpace;
+        private System.Windows.Forms.Button mno6;
+        private System.Windows.Forms.Button pqrs7;
+        private System.Windows.Forms.Button wxyz9;
+        private System.Windows.Forms.Button StarPlus;
+        private System.Windows.Forms.Button Hesh;
+    }
+}
+
---
+++ Homework_WinForms_Phone/Homework_WinForms_Phone/Phone.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_WinForms_Phone
+{
+    public partial class Phone : Form
+    {
+
+        public Phone()
+        {
+            InitializeComponent();
+
+        }
+        //Library library = new Library();
+        public int i;
+        public int k;
+        public int t;
+        public int click = 0;
+        public int click2 = 1;
+
+        public bool BigRu = false;
+        public bool BigEn = false;
+
+        public bool Ru = false;
+        public bool En = true;
+        public int EnOrRu = 0;
+
+        public bool Number = false;
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hesh_Click(object sender, EventArgs e)
+        {
+            if (En == true)
+            {
+                if (BigEn == false)
+                    BigEn = true;
+                else if (BigEn == true)
+                {
+                    Ru = true;
+                    En = false;
+                    BigEn = false;
+                }
+            }
+            else if (Ru == true)
+            {
+                if (BigRu == false)
+                    BigRu = true;
+                else if (BigRu == true)
+                {
+                    En = false;
+                    Ru = false;
+                    BigRu = false;
+                    Number = true;
+                }
+            }
+            else if(Number==true)
+            {
+                En = true;
+                Number = false;
+            }
+        }
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void One_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            litera(i);
+        }
+
+        private void abc2_Click(object sender, EventArgs e)
+        {           
+            i = 1;
+            litera(i);
+            
+    }
+
+        private void def3_Click(object sender, EventArgs e)
+        {
+            i = 2;
+            litera(i);
+        }
+
+        private void ghi4_Click(object sender, EventArgs e)
+        {
+            i = 3;
+            litera(i);
+        }
+
+        private void jkl5_Click(object sender, EventArgs e)
+        {
+            i = 4;
+            litera(i);
+        }
+
+        private void mno6_Click(object sender, EventArgs e)
+        {
+            i = 5;
+            litera(i);
+        }
+
+        private void pqrs7_Click(object sender, EventArgs e)
+        {
+            i = 6;
+            litera(i);
+        }
+
+        private void tuv8_Click(object sender, EventArgs e)
+        {
+            i = 7;
+            litera(i);
+        }
+
+        private void wxyz9_Click(object sender, EventArgs e)
+        {
+            i = 8;
+            litera(i);
+        }
+
+        void litera(int n)
+        {
+            //if (n == 0)
+            //{
+            //    if (Number != true)
+            //        n--;
+            //}
+
+            if (k != n)
+            {
+                click = 0;
+                click2 = 1;
+            }
+            k = n;
+
+           
+
+            char[][] en = new char[16][];
+            en[0] = new char[] { 'a', 'b', 'c' };
+            en[1] = new char[] { 'd', 'e', 'f' };
+            en[2] = new char[] { 'g', 'h', 'i' };
+            en[3] = new char[] { 'j', 'k', 'l' };
+            en[4] = new char[] { 'm', 'n', 'o' };
+            en[5] = new char[] { 'p', 'q', 'r', 's' };
+            en[6] = new char[] { 't', 'u', 'v' };
+            en[7] = new char[] { 'w', 'x', 'y', 'z' };
+            en[8] = new char[] { 'A', 'B', 'C' };
+            en[9] = new char[] { 'D', 'E', 'F' };
+            en[10] = new char[] { 'G', 'H', 'I' };
+            en[11] = new char[] { 'J', 'K', 'L' };
+            en[12] = new char[] { 'M', 'N', 'O' };
+            en[13] = new char[] { 'P', 'Q', 'R', 'S' };
+            en[14] = new char[] { 'T', 'U', 'V' };
+            en[15] = new char[] { 'W', 'X', 'Y', 'Z' };
+
+            char[][] ru = new char[16][];
+            ru[0] = new char[] { 'а', 'б', 'в', 'г' };
+            ru[1] = new char[] { 'д', 'е', 'ж', 'з' };
+            ru[2] = new char[] { 'и', 'й', 'к', 'л' };
+            ru[3] = new char[] { 'м', 'н', 'о', 'п' };
+            ru[4] = new char[] { 'р', 'с', 'т', 'у' };
+            ru[5] = new char[] { 'ф', 'х', 'ц', 'ч' };
+            ru[6] = new char[] { 'ш', 'щ', 'ъ', 'ы' };
+            ru[7] = new char[] { 'ь', 'э', 'ю', 'я' };
+            ru[8] = new char[] { 'А', 'Б', 'В', 'Г' };
+            ru[9] = new char[] { 'Д', 'Е', 'Ж', 'З' };
+            ru[10] = new char[] { 'И', 'Й', 'К', 'Л' };
+            ru[11] = new char[] { 'М', 'Н', 'О', 'П' };
+            ru[12] = new char[] { 'Р', 'С', 'Т', 'У' };
+            ru[13] = new char[] { 'Ф', 'Х', 'Ц', 'Ч' };
+            ru[14] = new char[] { 'Ш', 'Щ', 'Ъ', 'Ы' };
+            ru[15] = new char[] { 'Ь', 'Э', 'Ю', 'Я' };
+
+            int[] number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            char[] starplus = new char[] { '*', '+' };
+            
+            char[] symbol = new char[] { ',', '.', '!', '?', '/', '@'};
+
+            click++;
+            if (Number == true && n<20)
+            {
+                display.Text += number[n];
+            }
+            else if (n > 0 && n < 10)
+            {
+                n--;
+
+                if (BigRu == true || BigEn == true)
+                    n += 8;
+
+                if (En == true)
+                {
+                    if (n == 5 || n == 7 || n == 13 || n == 15)
+                    {
+                        if (click == 5)
+                            click = 1;
+                    }
+                    else
+                    {
+                        if (click == 4)
+                            click = 1;
+                    }
+                }
+                else if (Ru == true)
+                {
+                    if (click == 5)
+                        click = 1;
+                }
+                if (click2 > 1)
+                    Backspace();
+                if (En == true)
+                    display.Text += en[n][click - 1];
+                else if (Ru == true)
+                    display.Text += ru[n][click - 1];
+                click2 = 2;
+            }
+            else if (n == 0)
+            {
+                if (click == 7)
+                    click = 1;
+                if (click2 > 1)
+                    Backspace();
+                display.Text += symbol[click - 1];
+                click2 = 2;
+            }
+            
+            if (n == 20)
+            {
+                if (click == 3)
+                    click = 1;
+                if (click2 > 1)
+                    Backspace();
+                display.Text += starplus[click - 1];
+                click2 = 2;
+            }
+            
+        }
+
+        public void Backspace()
+        {
+            if (display.Text.Length > 0)
+            {
+                display.Text = display.Text.Remove(display.Text.Length - 1);
+            }
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            Backspace();
+        }
+
+        private void StarPlus_Click(object sender, EventArgs e)
+        {
+            i = 20;
+            litera(i);
+        }
+
+        private void ZeroSpace_Click(object sender, EventArgs e)
+        {
+            if (Number == false)
+            {
+                display.Text += " ";
+                click2 = 1;
+                click = 0;
+            }
+            else
+                display.Text += '0';
+        }
+    }
+
+}
